972ca81ea7be10514af6320b5e082a70f7a7f047
Rbn3D <ruben3d3@gmail.com>
2021-08-12 16:15:17 +0200

    Adjust power/torque and tire traction formulas. Code cleanup.

--- InverseTorqueSP/InverseTorqueSP/InverseTorqueSP.cs
+++ InverseTorqueSP/InverseTorqueSP/InverseTorqueSP.cs
@@ -48,11 +48,11 @@ namespace InverseTorque
 
             GripIdleScale       = Settings.GetValue<float>("SETTINGS", "GripIdleScale",       1.00f);
             GripSlideScaleMax   = Settings.GetValue<float>("SETTINGS", "GripSlideScaleMax",   0.985f);
-            GripSlideScaleMin   = Settings.GetValue<float>("SETTINGS", "GripSlideScaleMin",   0.9475f);
-            PowerSlideScale     = Settings.GetValue<float>("SETTINGS", "PowerSlideScale",     1.25f);
+            GripSlideScaleMin   = Settings.GetValue<float>("SETTINGS", "GripSlideScaleMin",   0.955f);
+            PowerSlideScale     = Settings.GetValue<float>("SETTINGS", "PowerSlideScale",     1.60f);
             TorqueSlideScale    = Settings.GetValue<float>("SETTINGS", "TorqueSlideScale",    1.60f);
-            OversteerIncrement  = Settings.GetValue<float>("SETTINGS", "OversteerIncrement",  0.25f);
-            UndersteerIncrement = Settings.GetValue<float>("SETTINGS", "UndersteerIncrement", 0.00f);
+            OversteerIncrement  = Settings.GetValue<float>("SETTINGS", "OversteerIncrement",  0.20f);
+            UndersteerIncrement = Settings.GetValue<float>("SETTINGS", "UndersteerIncrement", 0.10f);
 
             //curveMinTraction = new BezierCurve(new Point(0f, 0f), new Point(0.45f, 0.10f), new Point(0.7f, 0.7f), new Point(1f, 1f));
             //curveMaxTraction = new BezierCurve(new Point(0f, 0f), new Point(0.45f, 0.12f), new Point(0.66f, 0.8f), new Point(1f, 1f));
@@ -160,90 +160,42 @@ namespace InverseTorque
                     float longitudinalForce = gForce.Y;
                     float lateralForce = gForce.X;
 
-                    float absLongF = Math.Abs(longitudinalForce);
+                    //float absLongF = Math.Abs(longitudinalForce);
                     float absLatF  = Math.Abs(lateralForce);
+                    //float absLatFsSq  = (float)Math.Pow(absLatF, 2f);
 
-                    float combPositiveF = Math.Max(Clamp(longitudinalForce, 0f, 1f), Clamp(absLatF, 0f, 1f));
-                    float combPositiveF2 = Math.Max(Clamp(longitudinalForce * 0.5f, 0f, 0.5f), Clamp(absLatF, 0f, 1f));
+                    //float combPositiveF = Math.Max(Clamp(longitudinalForce, 0f, 1f), Clamp(absLatF, 0f, 1f));
+                    //float combPositiveF2 = Math.Max(Clamp(longitudinalForce * 0.5f, 0f, 0.5f), Clamp(absLatF, 0f, 1f));
 
 
-                    float auxForwAc = Clamp(longitudinalForce, 0f, 1f);
+                    float auxForwAc = Math.Max(0f, longitudinalForce);
+                    float auxForwAc03 = Clamp(longitudinalForce, 0f, 0.4f);
+                    float auxForwAc06 = Clamp(longitudinalForce, 0f, 0.7f);
                     float auxBackAc = map(longitudinalForce, 0f, -1f, 0f, 1f, true);
-                    float clampedLatF = Clamp(absLatF, 0f, 1f);
-                    float combCustom = Math.Max(auxForwAc, clampedLatF) * (1f - auxBackAc);
-                    float combCustom2 = Math.Max(auxForwAc, clampedLatF) * map(1f - auxBackAc, 0f, 1f, 0.5f, 1f);
+                    float combCustom = Math.Max(auxForwAc, absLatF) * (1f - auxBackAc);
 
-                    //float angle = Vector3.Angle(v.ForwardVector, v.Velocity.Normalized);
-                    //float multGrip = 0f;
-                    //float tracFact = 0f;
-                    //if (angle < 90)
-                    //{
-                    //multGrip = map(angle, 4f, 90, 1f, 3.0f, true);
-                    //tracFact = map(angle, 0f, 90, 0f, 1.0f, true);
-                    //}
-                    //else
-                    //{
-                    //    multGrip = Scaler * InitialGrip; //(float)Math.Round(map(angle, 180f, 90f, 1f, Scaler * grip, true), 2);
-                    //}
-
-                    //Vector3 vehAngularVelocity = GTA.Native.Function.Call<Vector3>(Hash.GET_ENTITY_ROTATION_VELOCITY, v);
-
-                    //float veolcityFactor = Easing.EaseOutCubic01(map(vehAngularVelocity.Length(), 0f, 5f, 0f, 1f, true));
-                    //float angleFactor = Easing.EaseOutCubic01(map(angle, 2.5f, 90f, 0f, 1f, true));
-
-                    //veolcityFactor *= map(v.Velocity.Length(), 0, 4, 0, 1, true);
-                    //angleFactor *= map(v.Velocity.Length(), 0, 4, 0, 1, true);
-
-                    //float finalFactor = Math.Max(veolcityFactor, angleFactor);
-
-                    //float auxVelo = map((finalFactor - prevFactVelo), 0f, 0.1f, 1f, 100f, true);
-                    //float auxAngle = map((finalFactor - prevFactAngle), 0f, 0.1f, 1f, 100f, true);
-
-                    //longLastingFactVelo = Lerp(longLastingFactVelo, veolcityFactor, auxVelo * Function.Call<float>(Hash.GET_FRAME_TIME));
-                    //longLastingFactAngle = Lerp(longLastingFactAngle, veolcityFactor, auxAngle * Function.Call<float>(Hash.GET_FRAME_TIME));
-
-                    //longLastingFact = Math.Max(longLastingFactVelo, longLastingFactAngle);
-
-                    //float SqLongLastingFact = (float)Math.Pow(longLastingFact, 2.0f);
-
-
+                    float combCustom2  = Math.Max(auxForwAc, absLatF) * map(1f - auxBackAc, 0f, 1f, 0.5f, 1f);
+                    float combCustom2L = Math.Max(auxForwAc03, absLatF);
+                    float combCustom3L = Math.Max(auxForwAc06, absLatF);
+                    float combCustom3Lm = Math.Max(auxForwAc06, absLatF) * (1f - auxBackAc);
 
                     var VHandling = v.HandlingData;
 
                     if (InverseTorqueDebug)
                     {
-                        //float InputTrhottle = GTA.Game.GetControlValueNormalized(Control.VehicleAccelerate);
-                        //float realThrottle = v.GetVehicleRealThrotle();
-
-                        //GTA.UI.Screen.ShowSubtitle("~n~~w~x" + Math.Round(InputTrhottle, 2).ToString() + " " + "~n~~w~x" + Math.Round(realThrottle, 2).ToString(), 500);
                         GTA.UI.Screen.ShowSubtitle("~n~~w~ X" + Math.Round(gForce.X, 2).ToString() + " " + "~n~~w~ Y" + Math.Round(gForce.Y, 2).ToString(), 500);
                     }
 
-
-                    //VHandling.TractionCurveMax = InitialTractionCurveMax * 1.005f;
-                    VHandling.TractionCurveMax = Lerp(InitialTractionCurveMax * GripIdleScale, InitialTractionCurveMax * GripSlideScaleMax, combCustom2);
-                    VHandling.TractionCurveMin = Lerp(InitialTractionCurveMin * GripIdleScale, InitialTractionCurveMin * GripSlideScaleMin, combCustom2);
+                    VHandling.TractionCurveMax = Lerp(InitialTractionCurveMax * GripIdleScale, InitialTractionCurveMax * GripSlideScaleMax, Powf(combCustom2, 1.5f));
+                    VHandling.TractionCurveMin = Lerp(InitialTractionCurveMin * GripIdleScale, InitialTractionCurveMin * GripSlideScaleMin, Powf(combCustom, 1.5f));
 
                     float auxSlideCamberStiffness = Lerp(InitialCamberStiffness, -0.10f, OversteerIncrement);
                     float auxIdleCamberStiffness  = Lerp(InitialCamberStiffness,  0.10f, UndersteerIncrement);
 
-                    VHandling.CamberStiffness = Lerp(auxIdleCamberStiffness, auxSlideCamberStiffness, combCustom);
+                    VHandling.CamberStiffness = Lerp(auxIdleCamberStiffness, auxSlideCamberStiffness, combCustom3Lm);
 
-                    //float easedThrotle = Easing.EaseOutCubic01(v.ThrottlePower);
-
-                    //v.EngineTorqueMultiplier = 1f + ((float)curveTorqueMult.GetY(longLastingFact));
-                    //v.EnginePowerMultiplier = 1f + ((float)curvePowerMult.GetY(longLastingFact));
-                    v.EnginePowerMultiplier  = Lerp(1f, PowerSlideScale, absLatF);
-                    v.EngineTorqueMultiplier = Lerp(1f, TorqueSlideScale, combCustom);
-
-                    //if (InverseTorqueDebug)
-                    //{
-                    //    //GTA.UI.Screen.ShowSubtitle("~n~~w~x" + Math.Round(finalFactor, 2).ToString() + " " + "~n~~w~x" + Math.Round(longLastingFact, 2).ToString(), 500);
-                    //}
-
-                    //prevFact = finalFactor;
-                    //prevFactVelo = veolcityFactor;
-                    //prevFactAngle = angleFactor;
+                    v.EnginePowerMultiplier  = Lerp(1f, PowerSlideScale, Powf(combCustom2L, 1.7f));
+                    v.EngineTorqueMultiplier = Lerp(1f, TorqueSlideScale, Powf(combCustom3L, 1.7f));
                 }
                 else
                 {
@@ -263,13 +215,6 @@ namespace InverseTorque
             }
             else
             {
-                //prevFact = 0f;
-                //prevFactVelo = 0f;
-                //prevFactAngle = 0f;
-                //longLastingFact = 0f;
-                //longLastingFactVelo = 0f;
-                //longLastingFactAngle = 0f;
-
                 VehicleExtensions.ClearGForceBuffer();
             }
 
@@ -337,7 +282,10 @@ namespace InverseTorque
             return a + (b - a) * t;
         }
 
-
+        public static float Powf(float value, float exp)
+        {
+            return (float)Math.Pow(value, exp);
+        }
 
         /// TOOLS ///
         void LoadSettings()
